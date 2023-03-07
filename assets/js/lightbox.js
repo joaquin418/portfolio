4ed3cfa7de6df5164a1f5ca38c7ffd00eebbef7c
joaquin418 <125749963+joaquin418@users.noreply.github.com>
2023-03-07 15:04:27 +0800

    Add files via upload

---
+++ assets/js/lightbox.js
@@ -0,0 +1,2 @@
+var venobox =new VenoBox({selector:".venobox", infinigall:"false", numeration:"true", spinner:"flow", autoplay:"false", navTouch: "true", navKeyboard:"true", navigation:"true",});
+    
