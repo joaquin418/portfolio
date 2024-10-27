b5865bcc87b750f5f74f61abbad0504b4df261bc
joaquin418 <125749963+joaquin418@users.noreply.github.com>
2024-10-27 15:14:52 +0800

    Delete assets/js directory

--- assets/js/lightbox.js
+++
@@ -1,2 +0,0 @@
-var venobox =new VenoBox({selector:".venobox", infinigall:"false", numeration:"true", spinner:"flow", autoplay:"false", navTouch: "true", navKeyboard:"true", navigation:"true",});
-    
